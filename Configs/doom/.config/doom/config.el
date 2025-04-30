dbd458ecf5f32eb6de3dfc6102fd0a762e6a8c43
Bruno Bonatto <bruno.bonatto@simfrete.com.br>
2025-04-30 17:58:04 -0300

    python uv

--- Configs/doom/.config/doom/config.el
+++ Configs/doom/.config/doom/config.el
@@ -100,7 +100,8 @@
 (after! python
   (setq! dap-python-debugger 'debugpy)
   (add-hook! python-mode
-             '(prettify-symbols-mode rainbow-delimiters-mode-enable))
+             '(prettify-symbols-mode rainbow-delimiters-mode-enable)
+             #'uv-mode-auto-activate-hook)
   (setq python-shell-completion-native-enable nil))
 
 (after! (:and python python-pytest)

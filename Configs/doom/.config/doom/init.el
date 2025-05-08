75a092d7ca094c606bc3d35e89ef30254f564c38
Bruno Bonatto <bfbonatto@gmail.com>
2025-05-07 22:15:49 -0300

    high-level languages

--- Configs/doom/.config/doom/init.el
+++ Configs/doom/.config/doom/init.el
@@ -129,7 +129,7 @@
        ;; +lsp
        ;; +tree-sitter)           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
-       ;;coq               ; proofs-as-programs
+       coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
@@ -170,7 +170,9 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       (nix
+        +lsp
+        +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
         +pomodoro)               ; organize your plain life in plain text
